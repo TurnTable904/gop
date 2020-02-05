2657f5fe176c199e0805826e3dd003ce15fc14b5
Emiliano Balbuena <edgebal@gmail.com>
2020-02-05 17:29:55 -0300

    (chore): Check dependencies, hide niin package output

--- local/cli.js
+++ local/cli.js
@@ -1,17 +1,31 @@
-return require('yargs')
-  .option('verbose', {
-    description: 'Verbose output',
-    boolean: true
-  })
-  .option('silent', {
-    description: 'Silent output',
-    boolean: true
-  })
-  .command('up', 'Start the containers', require('./commands/up'))
-  .command('down', 'Stop the containers', require('./commands/down'))
-  .command('restart', 'Restart the containers', require('./commands/restart'))
-  .command('install', 'Installs and provisions the compose stack', require('./commands/install'))
-  .demandCommand(1, 'Please, specify a command.')
-  .help()
-  .strict()
-  .argv;
+const yargs = require('yargs');
+const getMissingDeps = require('./helpers/get-missing-deps');
+
+return (async () => {
+  const missingDeps = await getMissingDeps();
+
+  if (missingDeps.length) {
+    process.stderr.write(
+      `FATAL: Missing dependencies: ${missingDeps.join(', ')}\n`
+    );
+    return process.exit(1);
+  }
+
+  return yargs
+    .option('verbose', {
+      description: 'Verbose output',
+      boolean: true
+    })
+    .option('silent', {
+      description: 'Silent output',
+      boolean: true
+    })
+    .command('up', 'Start the containers', require('./commands/up'))
+    .command('down', 'Stop the containers', require('./commands/down'))
+    .command('restart', 'Restart the containers', require('./commands/restart'))
+    .command('install', 'Installs and provisions the compose stack', require('./commands/install'))
+    .demandCommand(1, 'Please, specify a command.')
+    .help()
+    .strict()
+    .argv;
+})();
