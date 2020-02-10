6c39478091ac374fb678fb0929034a0646adc6d2
Emiliano Balbuena <edgebal@gmail.com>
2020-02-10 10:41:27 -0300

    (chore): Local Serve PoC command

---
+++ local/commands/local-serve.js
@@ -0,0 +1,26 @@
+const exec = require('../lib/exec');
+
+module.exports.handler = async argv => {
+  if (argv.verbose) {
+    // TODO: Handle this
+  } else if (argv.silent) {
+    // TODO: Handle this
+  }
+
+  const subprocess = exec.in('front', 'npm', ['run', 'serve:dev'], {
+    env: {
+      ...process.env,
+      ENGINE_SECURE: '',
+      ENGINE_HOST: 'localhost',
+      ENGINE_PORT: '8080',
+      NODE_OPTIONS: '--max_old_space_size=4096'
+    }
+  });
+
+  subprocess.stdout.pipe(process.stdout);
+  subprocess.stderr.pipe(process.stderr);
+
+  await subprocess;
+};
+
+module.exports.builder = {};
